6249734ede6314db35ab59416c52c588df9cb3b8
François Devantay <francois.devantay@studentfr.ch>
2022-12-06 09:28:29 +0100

    finsih the other pages (404 to be honest)

--- dist/index.18dbc454.js
+++ dist/index.18dbc454.js
@@ -562,6 +562,30 @@ triggersContact.forEach((triggerContact)=>{
         hoverBackground.classList.remove("active-hover-bg");
     });
 });
+var menuTrigger = document.querySelector(".menu-trigger");
+var menu = document.querySelector(".menu");
+console.log(menuTrigger, menu);
+menuTrigger.addEventListener("click", ()=>{
+    menu.classList.toggle("menuOpen");
+}); // 1
+ // testfunction = function () {
+ //   console.log("hello");
+ // };
+ // openMenu = function () {
+ //   menuTrigger.addEventListener("click", "testfunction");
+ // };
+ // // 2
+ // openMenu = function () {
+ //   menuTrigger.addEventListener("click", function () {
+ //     console.log("hello");
+ //   });
+ // };
+ // // 3
+ // openMenu = function () {
+ //   menuTrigger.addEventListener("click", () => {
+ //     console.log("hello");
+ //   });
+ // };
 
 },{}]},["jQVXF","1SICI"], "1SICI", "parcelRequire0300")
 
--- src/js/main.js
+++ src/js/main.js
@@ -39,3 +39,36 @@ triggersContact.forEach((triggerContact) => {
     hoverBackground.classList.remove("active-hover-bg");
   });
 });
+
+var menuTrigger = document.querySelector(".menu-trigger");
+var menu = document.querySelector(".menu");
+console.log(menuTrigger, menu);
+
+menuTrigger.addEventListener("click", () => {
+  menu.classList.toggle("menuOpen");
+});
+
+// 1
+// testfunction = function () {
+//   console.log("hello");
+// };
+
+// openMenu = function () {
+//   menuTrigger.addEventListener("click", "testfunction");
+// };
+
+// // 2
+
+// openMenu = function () {
+//   menuTrigger.addEventListener("click", function () {
+//     console.log("hello");
+//   });
+// };
+
+// // 3
+
+// openMenu = function () {
+//   menuTrigger.addEventListener("click", () => {
+//     console.log("hello");
+//   });
+// };
